bce5f9e630051459c8fcd4133428e746529cd3e0
FaisalGaur <39118766+FaisalGaur@users.noreply.github.com>
2019-12-16 18:06:34 +0530

    Update datacompare.py

--- datacompare.py
+++ datacompare.py
@@ -1,17 +1,28 @@
 import pandas as pd
-import numpy as np
 from functools import reduce
 
-DateWise = pd.read_excel('WYNBRANDS.xls', sheet_name='DateWise')
-Ledger = pd.read_excel('WYNBRANDS.xls', sheet_name='Ledger')
-Warehouse = pd.read_excel('WYNBRANDS.xls', sheet_name='Stock_Avail_By_Warehouse')
 
-final_df = [DateWise, Ledger, Warehouse]
+def all_same(items):
+    return all(x == items[0] for x in items)
 
-Results = reduce(lambda left, right: pd.merge(left, right, on='Product ID', how='outer'), final_df)
 
-Results['Match'] = np.where(((Results['Qty Datewise'] == Results['Qty Ledger']) &
-                            (Results['Qty Datewise'] == Results['Qty Warehouse']) &
-                            (Results['Qty Ledger'] == Results['Qty Warehouse'])), 'True', 'False')
-Results.to_csv("Results.csv", sep=',')
+def read_data(filename, sheet):
+    data_frame = pd.read_excel(filename, sheet_name=sheet)
+    return data_frame
+
+
+def calculate_results(final_frame, common_field, result_field):
+    result = reduce(lambda left, right: pd.merge(left, right, on=common_field, how='outer'), final_frame)
+    result[result_field] = (result.iloc[:, 1:].eq(result.iloc[:, 1], axis=0)).all(1)
+    return result
+
+
+if __name__ == '__main__':
+    DateWise = read_data('WYNBRANDS.xls', 'DateWise')
+    Ledger = read_data('WYNBRANDS.xls', 'Ledger')
+    Warehouse = read_data('WYNBRANDS.xls', 'Stock_Avail_By_Warehouse')
+    final_df = [DateWise, Ledger, Warehouse]
+    Results = calculate_results(final_df, 'Product ID', 'Match Result')
+    Results.to_csv("Results.csv", sep=',')
+
 
